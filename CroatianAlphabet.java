6e74871b69fbcc3495f2f85100eda0776b52fcfc
kimseongmin0301 <pose1358@naver.com>
2023-06-08 13:36:52 +0900

    [백준 2941] 크로아티아 알파벳

---
+++ CroatianAlphabet.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://www.acmicpc.net/problem/2941
+ * 백준 2941
+ * 크로아티아 알파벳
+ */
+public class CroatianAlphabet {
+        public static void main(String[] args) throws IOException {
+            String [] alpha = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String b = br.readLine();
+
+            for(int i=0; i<alpha.length; i++) {
+                if(b.contains(alpha[i])) {
+                    b = b.replaceAll(alpha[i], "o");
+                }
+            }
+
+            System.out.println(b.length());
+        }
+}
